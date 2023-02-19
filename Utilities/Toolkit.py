97568b331e5f1d21d704a8e590dd3898bb13edf9
Adrian <ortunoa@outlook.com>
2023-02-19 06:14:06 -0700

    fixed port auto detection

--- Utilities/Toolkit.py
+++ Utilities/Toolkit.py
@@ -1,7 +1,11 @@
 from serial.tools import list_ports
+import obd
 
 def getPortName():
     ports = list(set([item for item in list_ports.comports()]))
+    ports = [str(x.name) for x in ports]
+    
     for port in ports:
-        if port.manufacturer == 'FTDI':
-            return str(port.device)
+        connection = obd.OBD(port)
+        if "ELM327" in str(connection.interface):
+            return port
