58ed05f9c29c6607c9b5f4a3c20e590ca2e2aba4
Adrian <ortunoa@outlook.com>
2023-04-08 08:45:14 -0600

    wrote code to pull models from Firebase

---
+++ getModel.py
@@ -0,0 +1,28 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+vehicle_id = '1'
+
+cred = credentials.Certificate("digitaltwinobdii-firebase-adminsdk-dob8j-cbf5404441.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://digitaltwinobdii-default-rtdb.firebaseio.com/'
+})
+
+def get_latest_model_with_highest_mse(vehicle_id):
+    ref = db.reference('digitalTwinModels')
+    query = ref.order_by_child('Identifiers/Id').equal_to(vehicle_id)
+    result = query.get()
+
+    if result:
+        latest_model_key, latest_model_data = max(result.items(), key=lambda x: (x[1]['DateTime'], x[1]['Scores']['MSE']))
+        print(f"Latest model with the highest MSE score for vehicle ID '{vehicle_id}':\n{latest_model_data}")
+        return latest_model_data
+    else:
+        print(f"No model found for vehicle ID '{vehicle_id}'")
+        return None
+    
+
+model = get_latest_model_with_highest_mse(vehicle_id)
+
+print(model)
